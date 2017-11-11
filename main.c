21e1268f719cbab4555f30776ca2044501283551
Carlos F. Ferry <carlos.ferry@gmail.com>
2017-11-11 17:56:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 /*
  * Virtual Memory Simulator Project
+ *
  * Eric Kuha
  * Carlos Ferry
  * Jared Willard
  *
  * Three page fault handlders with three different
  * eviction policies.
- * 1. FIFO, evicts on a first-in-first-out policy
- * 2. Random, evicts pages completely randomly
- * 3. Custom, evicts pages on a random, second-chance basis, favoring
- *      pages that have the dirty bit set.
+ *
+ *  FIFO: evicts on a first-in-first-out policy.
+ *
+ *  Random: evicts pages completely randomly.
+ *
+ *  Custom: evicts pages on a random, second-chance basis, favoring
+ *          pages that have the dirty bit set. We have named this
+ *          algorithm: "Second-try-fifo", or "2ndfifo".
  */
 
 #include "page_table.h"
@@ -23,32 +28,41 @@
 #include <time.h>
 
 /* Handy Bool Typedef */
+
 typedef enum { false, true } bool;  
+
 
 static int TotalPages = 0;
 static int TotalFrames = 0; 
 
 /* Pointers to data structures that the fault handlers need */
+
 char *physmem = NULL;
+
 struct disk *disk = NULL;
 
 /* Handles appending file */
+
 FILE *Output = NULL;
 
-/* Algo option provided by user. */
-int UserOption = 0;
+/* Algo option provided by user in argc. */
+
+static int UserOption = 0;
 
 /* Fault counters */
+
 static int Faults = 0;
 static int Writes = 0;
 static int Reads = 0;
 
-/* counts number of frames occupied (random/custom) */ 
+/* Counts number of frames occupied (random/custom) */ 
+
 static int counter = 0; 
 
 int *FrameArray;
 
 /* FIFO data structures */
+
 typedef struct FifoNode
 {
     int data;
@@ -68,7 +82,8 @@ void setup_fifo(int cap);
 void push_fifo(int page);
 int pop_fifo(void);
 
-/* set up for random algorithm. */
+/* Set up for random algorithm. */
+
 void setup_frame_array(int cap);
 
 int cust_get_frame(struct page_table *pt);
@@ -80,52 +95,68 @@ void FrameworkSetup(struct page_table *pt);
  * Very basic fault handler which evicts pages based
  * solely on position in queue.
  */
+
 void fifo_fault_handler( struct page_table *pt, int page)
 {
-    // Increment page fault counter
+    /* Increment page fault counter. */
+    
     Faults++;
 
-    // Get information about the page.
+    /* Get information about the page. */
+    
     int frame, bits;
+    
     page_table_get_entry(pt, page, &frame, &bits);
 
-    // if this page is not in memory
-    if(!bits&PROT_READ)
+    /* if this page is not in memory */
+    
+    if (!bits&PROT_READ)
     {
-        // There will be a read
+        /* There will be a read. */
+        
         Reads++;
-        // If not all frames are being used
-        if(fifoq->size < fifoq->capacity)
+        
+        /* If not all frames are being used. */
+        
+        if (fifoq->size < fifoq->capacity)
         {
-            // It will take the next frame in the queue.
+            /* It will take the next frame in the queue. */
+            
             frame = fifoq->size;
             disk_read(disk, page, &physmem[frame*PAGE_SIZE]);
         }
         else
         {
-            // We need to evict the front page in the queue.
+            /* We need to evict the front page in the queue. */
+            
             int out_page = pop_fifo();
             int out_frame, out_bits;
             page_table_get_entry(pt, out_page, &out_frame, &out_bits);
 
-            // If the evicted frame is dirty, write it to the disk
-            if(out_bits&PROT_WRITE)
+            /* If the evicted frame is dirty, write it to the disk */
+            
+            if (out_bits&PROT_WRITE)
             {
                 Writes++;
                 disk_write(disk, out_page, &physmem[out_frame*PAGE_SIZE]);
             }
+            
             disk_read(disk, page, &physmem[out_frame*PAGE_SIZE]);
             page_table_set_entry(pt, out_page, 0, 0);
             frame = out_frame;
         }
+        
         push_fifo(page);
         bits = PROT_READ;
     }
-    // else, make it dirty
+    
+    /* else, make it dirty */
+    
     else
     {
         bits = PROT_READ|PROT_WRITE;
     }
+
     page_table_set_entry(pt,page,frame,bits);
 
 }
@@ -134,72 +165,91 @@ void fifo_fault_handler( struct page_table *pt, int page)
  * FindPage finds a particular page in an array
  * Used by Custom and Random handlers
  */
+
 int FindPage(int* arr, int size, int key) 
 {
-    for (unsigned i = 0; i < size; i++) 
-    {
-        if (arr[i] == key)
-        {
-            return i;
-        }  
-    }
-    return -1;
+      for (unsigned i = 0; i < size; i++) 
+      {
+            if (arr[i] == key)
+            {
+                return i;
+            }    
+      }
+    
+      return -1;
 }
 
-
-/**
- * Custome Fault Handler
+/*
+ * Custom Fault Handler
  * Eviction policy is based on random but gives
  * a second chance to any frame with PROT_WRITE
  * bit set.
  */
+ 
 void cust_fault_handler(struct page_table *pt, int page)
 {
     Faults++;
 
-    // Get info about faulting frame
+    /* Get info about faulting frame. */
+    
     int frame, bits;
+    
     page_table_get_entry(pt, page, &frame, &bits);
 
-    // If page is not in memory
-    if(!bits&PROT_READ)
+    /* If page is not in memory. */
+    
+    if (!bits&PROT_READ)
     {
         Reads++;
-        // If we still have unallocated frames
-        if(counter < TotalFrames)
+        
+        /* If we still have unallocated frames. */
+        
+        if (counter < TotalFrames)
         {
-            FrameArray[counter] = page;
-            counter++;
-            disk_read(disk, page, &physmem[frame*PAGE_SIZE]);
+             FrameArray[counter] = page;
+             counter++;
+             disk_read(disk, page, &physmem[frame*PAGE_SIZE]);
         }
-        // Execute eviction policy
+        
+        /* Execute eviction policy */
+        
         else
         {
-            // Find a frame to evict here.
+            /* Find a frame to evict here. */
+            
             int out_page = cust_get_frame(pt);
             int out_frame, out_bits;
             page_table_get_entry(pt, out_page, &out_frame, &out_bits);
-            if(out_bits&PROT_WRITE)
+            
+            if (out_bits&PROT_WRITE)
             {
                 Writes++;
                 disk_write(disk, out_page, &physmem[out_frame*PAGE_SIZE]);
             }
+            
             disk_read(disk, page, &physmem[out_frame*PAGE_SIZE]);
             page_table_set_entry(pt, out_page, 0, 0);
             frame = out_frame;
             bits = PROT_READ;
-            // put the new page into the FrameArray
+            
+            /* put the new page into the FrameArray */
+            
             int frame_index = FindPage(FrameArray, TotalFrames, out_page);
             FrameArray[frame_index] = page;
         }
+        
         bits = PROT_READ;
     }
-    // else make it dirty
+    
+    /* else make it dirty */
+    
     else
     {
         bits = PROT_READ|PROT_WRITE;
     }
-    // Finally, set the page table entry
+   
+    /* Finally, set the page table entry. */
+    
     page_table_set_entry(pt,page,frame,bits);
 }
 
@@ -207,6 +257,7 @@ void cust_fault_handler(struct page_table *pt, int page)
  * Random Fault Handler
  * Chooses frames to evict purely randomly.
  */
+ 
 void random_fault_handler(struct page_table *pt, int page)
 {
     Faults++;
@@ -214,12 +265,15 @@ void random_fault_handler(struct page_table *pt, int page)
     int frame, bits;
     page_table_get_entry(pt, page, &frame, &bits);
 
-    // If page is not in memory
-    if(!bits&PROT_READ)
+    /* Checks If page is not in memory. */
+    
+    if (!bits&PROT_READ)
     {
         Reads++;
-        // If we still have unallocated frames
-        if(counter < TotalFrames)
+        
+        /* If we still have unallocated frames */
+        
+        if (counter < TotalFrames)
         {
             FrameArray[counter] = page;
             counter++;
@@ -230,7 +284,8 @@ void random_fault_handler(struct page_table *pt, int page)
             int out_page = rand() % TotalFrames;
             int out_frame, out_bits;
             page_table_get_entry(pt, out_page, &out_frame, &out_bits);
-            if(out_bits&PROT_WRITE)
+           
+            if (out_bits&PROT_WRITE)
             {
                 Writes++;
                 disk_write(disk, out_page, &physmem[out_frame*PAGE_SIZE]);
@@ -241,22 +296,26 @@ void random_fault_handler(struct page_table *pt, int page)
             frame = out_frame;
             bits = PROT_READ;
 
-            // Put new page into FrameArray
+            /* Put new page into FrameArray */
+            
             int frameIndex = FindPage(FrameArray, TotalFrames, out_page);
             FrameArray[frameIndex] = page;
         }
+        
         bits = PROT_READ;
     }
     else
     {
         bits = PROT_READ|PROT_WRITE;
     }
+    
     page_table_set_entry(pt,page,frame,bits);
 }
 
 /*
  * Naive page fault handler
  */
+ 
 void page_fault_handler( struct page_table *pt, int page )
 {
     printf("page fault on page #%d\n",page);
@@ -328,13 +387,17 @@ int main(int argc, char *argv[])
     {
         pt = page_table_create( npages, nframes, fifo_fault_handler);
     }
+    
     /* Handles Random */
+    
     else if (UserOption == 1)
     {
         pt = page_table_create( npages, nframes, random_fault_handler);
         FrameworkSetup(pt);
     }
-    // Custom
+    
+    /* Custom */
+    
     else if (UserOption == 3)
     {
         pt = page_table_create( npages, nframes, cust_fault_handler);
@@ -358,25 +421,32 @@ int main(int argc, char *argv[])
     physmem = page_table_get_physmem(pt);
 
 
-    if(!strcmp(program,"sort")) {
+    if(!strcmp(program,"sort")) 
+    {
         sort_program(virtmem,npages*PAGE_SIZE);
         outputFile = "sort_results.csv";
 
-    } else if(!strcmp(program,"scan")) {
+    } 
+    else if(!strcmp(program,"scan")) 
+    {
         scan_program(virtmem,npages*PAGE_SIZE);
         outputFile = "scan_results.csv";
 
-    } else if(!strcmp(program,"focus")) {
+    } 
+    else if(!strcmp(program,"focus")) 
+    {
         focus_program(virtmem,npages*PAGE_SIZE);
         outputFile = "focus_results.csv";
 
-    } else {
+    } 
+    else 
+    {
         fprintf(stderr,"unknown program: %s\n",argv[3]);
     }
 
     Output = fopen(outputFile, "a");
 
-    if(NULL != Output)
+    if (NULL != Output)
     {
         fseek(Output, 0, SEEK_END);
         int size = ftell(Output);
@@ -404,6 +474,7 @@ int main(int argc, char *argv[])
  * based on a based capacity.
  * Initializes all indices to -1
  */
+ 
 void setup_frame_array(int cap)
 {
     FrameArray = (int *)malloc(cap * sizeof(int));
@@ -417,6 +488,7 @@ void setup_frame_array(int cap)
 /*
  * Creates FIFO queue.
  */
+ 
 void setup_fifo(int cap)
 {
     FifoQueue *q = (FifoQueue*)malloc(sizeof(FifoQueue));
@@ -430,12 +502,13 @@ void setup_fifo(int cap)
  * Pushes an int page number to the back
  * of the FIFO queue
  */
+
 void push_fifo(int page)
 {
     FifoNode *n = (FifoNode*)malloc(sizeof(FifoNode));
     n->data = page;
 
-    if(fifoq->size > 0)
+    if (fifoq->size > 0)
     {
         fifoq->rear->next = n;
         fifoq->rear = n;
@@ -445,6 +518,7 @@ void push_fifo(int page)
         fifoq->front = n;
         fifoq->rear = n;
     }
+    
     fifoq->size = fifoq->size + 1;
 }
 
@@ -452,6 +526,7 @@ void push_fifo(int page)
  * Pops an int page number from
  * the front of the FIFO queue
  */
+ 
 int pop_fifo(void)
 {
     FifoNode *n = fifoq->front;
@@ -470,6 +545,7 @@ int pop_fifo(void)
  * 2. If the PROT_EXEC bit is set, unset it and look again.
  * 3. If it is not set, return that page.
  */
+
 int cust_get_frame(struct page_table *pt)
 {
     while(1)
@@ -493,6 +569,7 @@ int cust_get_frame(struct page_table *pt)
  * Establishes some of the important globals for
  * the random and custom algorithms
  */
+
 void FrameworkSetup(struct page_table *pt)
 {
     TotalPages = page_table_get_npages(pt);
